2eef102b46d6a533d1189d9331fb0664820b1996
Perez Navarro <sdpz@gft.com>
2021-07-08 18:32:04 +0200

    Added information and Edge

--- mimodelo/trainer/train.py
+++ mimodelo/trainer/train.py
@@ -11,14 +11,13 @@ print(tf.__version__)
 
 The dataset is available from the [UCI Machine Learning Repository](https://archive.ics.uci.edu/ml/).
 
-### Get the data
-First download the dataset.
+
 """
+
+# Descargamos el dataset y lo procesamos con pandas
 
 dataset_path = keras.utils.get_file("auto-mpg.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
 dataset_path
-
-"""Import it using pandas"""
 
 column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight',
                 'Acceleration', 'Model Year', 'Origin']
@@ -26,117 +25,63 @@ dataset = pd.read_csv(dataset_path, names=column_names,
                       na_values = "?", comment='\t',
                       sep=" ", skipinitialspace=True)
 
-dataset.tail()
 
-# TODO: replace `your-gcs-bucket` with the name of the Storage bucket you created earlier
 BUCKET = 'gs://wells-test-classification'
 
-"""### Clean the data
-
-The dataset contains a few unknown values.
-To keep this initial tutorial simple drop those rows."""
+# Por facilitar el ejemplo, quitamos las filas con valores faltantes
 
 dataset = dataset.dropna()
 
-"""The `"Origin"` column is really categorical, not numeric. So convert that to a one-hot:"""
+# La columna "Origin" es de tipo categorica, el procedimiento con estas variables suele hacer un One-Hot Encoding
 
 dataset['Origin'] = dataset['Origin'].map({1: 'USA', 2: 'Europe', 3: 'Japan'})
 
 dataset = pd.get_dummies(dataset, prefix='', prefix_sep='')
 dataset.tail()
 
-"""### Split the data into train and test
-
-Now split the dataset into a training set and a test set.
-
-We will use the test set in the final evaluation of our model.
-"""
+# Dividimos el dataset en entrenamiento y test
 
 train_dataset = dataset.sample(frac=0.8,random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
 
-"""### Inspect the data
-
-Have a quick look at the joint distribution of a few pairs of columns from the training set.
-
-Also look at the overall statistics:
-"""
+# Por comprobar que ambos sets tienen una naturaleza "comun" se suelen mirar las estadisticas
 
 train_stats = train_dataset.describe()
 train_stats.pop("MPG")
 train_stats = train_stats.transpose()
 train_stats
 
-"""### Split features from labels
-
-Separate the target value, or "label", from the features. This label is the value that you will train the model to predict.
-"""
-
+# Separamos los targets para ambos sets de entrenamiento
 train_labels = train_dataset.pop('MPG')
 test_labels = test_dataset.pop('MPG')
 
-"""### Normalize the data
-
-Look again at the `train_stats` block above and note how different the ranges of each feature are.
-
-It is good practice to normalize features that use different scales and ranges. Although the model *might* converge without feature normalization, it makes training more difficult, and it makes the resulting model dependent on the choice of units used in the input.
-
-Note: Although we intentionally generate these statistics from only the training dataset, these statistics will also be used to normalize the test dataset. We need to do that to project the test dataset into the same distribution that the model has been trained on.
-"""
+# Normalizamos los dos sets con sus respectivas estadísticas, esto es importante pues si normalizamos el conjunto de entrenamiento teniendo en cuenta la estadística del conjunto de test tendríamos un caso de data leakeage.
 
 def norm(x):
-  return (x - train_stats['mean']) / train_stats['std']
+    return (x - train_stats['mean']) / train_stats['std']
 normed_train_data = norm(train_dataset)
 normed_test_data = norm(test_dataset)
 
-"""This normalized data is what we will use to train the model.
-
-Caution: The statistics used to normalize the inputs here (mean and standard deviation) need to be applied to any other data that is fed to the model, along with the one-hot encoding that we did earlier.  That includes the test set as well as live data when the model is used in production.
-
-## The model
-
-### Build the model
-
-Let's build our model. Here, we'll use a `Sequential` model with two densely connected hidden layers, and an output layer that returns a single, continuous value. The model building steps are wrapped in a function, `build_model`, since we'll create a second model, later on.
-"""
+# Construimos el modelo
 
 def build_model():
-  model = keras.Sequential([
+    model = keras.Sequential([
     layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
     layers.Dense(64, activation='relu'),
     layers.Dense(1)
-  ])
+    ])
 
-  optimizer = tf.keras.optimizers.RMSprop(0.001)
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
 
-  model.compile(loss='mse',
+    model.compile(loss='mse',
                 optimizer=optimizer,
                 metrics=['mae', 'mse'])
-  return model
+    return model
 
 model = build_model()
-
-"""### Inspect the model
-
-Use the `.summary` method to print a simple description of the model
-"""
-
 model.summary()
 
-"""Now try out the model. Take a batch of `10` examples from the training data and call `model.predict` on it.
-
-It seems to be working, and it produces a result of the expected shape and type.
-
-### Train the model
-
-Train the model for 1000 epochs, and record the training and validation accuracy in the `history` object.
-
-Visualize the model's training progress using the stats stored in the `history` object.
-
-This graph shows little improvement, or even degradation in the validation error after about 100 epochs. Let's update the `model.fit` call to automatically stop training when the validation score doesn't improve. We'll use an *EarlyStopping callback* that tests a training condition for  every epoch. If a set amount of epochs elapses without showing improvement, then automatically stop the training.
-
-You can learn more about this callback [here](https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/EarlyStopping).
-"""
+# Entrenamos el modelo con un callback de EarlyStopping para dejar de entrenar en caso de que la métrica a observar deje de evolucionar positivamente durante unas epocas de paciencia.
 
 model = build_model()
 
@@ -150,5 +95,5 @@ early_history = model.fit(normed_train_data, train_labels,
                     callbacks=[early_stop])
 
 
-# Export model and save to GCS
+# Guardamos el modelo en un bucket, esto es opcional.
 model.save(BUCKET + '/mimodelo/model')
